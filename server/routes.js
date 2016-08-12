e332a77a6e94293414545556ef140705d18c1933
Justin Lim <liminator1106@gmail.com>
2016-08-11 23:23:11 -0700

    api call to ig working

--- server/routes.js
+++ server/routes.js
@@ -7,8 +7,9 @@ module.exports = function(app) {
 
   app.get('/instagram', (req, res) => {
     axios.get('https://api.instagram.com/v1/locations/search?lat=34.054348&lng=-118.44390099999998&access_token=32648064.93ca2fa.baa7da1964c94c408a089d1376a3bc52&distance=1')
-    .then(function (data) {
-      console.log(data);
+    .then(function (response) {
+
+      res.send(response.data);
     })
     .catch(function (error) {
       console.log(error);
